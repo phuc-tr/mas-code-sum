c635a1b8931266f775ea6056f55eaf90d8dbcf38
Phuc Tran <tranthanhphuc042@gmail.com>
2026-04-12 21:19:13 +0700

    Remove chain_of_thought and prompt_tuned from methods registry

--- src/mas_code_sum/methods/__init__.py
+++ src/mas_code_sum/methods/__init__.py
@@ -1,4 +1,3 @@
-from .chain_of_thought import ChainOfThoughtSummarizer
 from .style_guided import StyleGuidedSummarizer
 from .codet5_summarizer import CodeT5Summarizer
 from .exact_copy import ExactCopySummarizer
@@ -7,7 +6,6 @@ from .few_shot_context_enriched import FewShotContextEnrichedSummarizer
 from .few_shot_critic import FewShotCriticSummarizer
 from .few_shot_random_context_enriched import FewShotRandomContextEnrichedSummarizer
 from .few_shot_llm import FewShotLLMSummarizer
-from .prompt_tuned import PromptTunedSummarizer
 from .zero_shot_context_enriched import ZeroShotContextEnrichedSummarizer
 from .zero_shot_llm import ZeroShotLLMSummarizer
 
@@ -20,8 +18,6 @@ REGISTRY = {
     "few_shot_critic": FewShotCriticSummarizer,
     "few_shot_random_context_enriched": FewShotRandomContextEnrichedSummarizer,
     "few_shot_asap": FewShotAsapSummarizer,
-    "chain_of_thought": ChainOfThoughtSummarizer,
-    "prompt_tuned": PromptTunedSummarizer,
     "codet5": CodeT5Summarizer,
     "style_guided": StyleGuidedSummarizer,
 }
